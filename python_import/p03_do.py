636163d7bd0ae3b8348753804e739d7f117028ff
leekyunghun <oh_pizza@naver.com>
2020-12-14 18:35:29 +0900

    python

---
+++ python_import/p03_do.py
@@ -0,0 +1,6 @@
+import p01_car          # .py를 import해서 안에 있는 코드를 실행가능
+import p02_tv
+
+print("=========================")
+p01_car.drive()
+p02_tv.watch()
---
+++ python_import/p21_car.py
@@ -0,0 +1,5 @@
+def drive():
+    print("운전하다")
+
+if __name__ == '__main__':  # import한 module(다른 .py) 말고 p21_car.py에 있는걸 실행하고싶을때
+    drive()
